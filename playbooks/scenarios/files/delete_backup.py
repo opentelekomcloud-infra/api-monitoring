6223cdfc480c548b5157c5e8ad1c90e317ad78d5
Artem Goncharov <artem.goncharov@gmail.com>
2019-07-30 09:35:54 +0200

    in delete backup check and wait if it is still creating

--- playbooks/scenarios/files/delete_backup.py
+++ playbooks/scenarios/files/delete_backup.py
@@ -12,6 +12,13 @@ for backup in conn.block_storage.backups():
         if backup_found:
             print('Backup with this name was already found, potentially'
                   'multiple')
+        if backup.status == 'creating':
+            for count in openstack.utils.iterate_timeout(
+                    timeout=300, message='Wait for backup status failed',
+                    wait=5):
+                backup = conn.block_storage.get_backup(backup.id)
+                if backup.status != 'creating':
+                    break
         backup_found = True
         conn.block_storage.delete_backup(backup.id)
         conn.block_storage.wait_for_delete(backup, interval=2, wait=300)
